e024550eb844a61385f5722de7ed93349456cb0f
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-06-19 10:01:06 +0700

    remove unused import for ChatMessage component

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState} from 'react'
 
 import {ChatInput} from './components/ChatInput.jsx';
-import { ChatMessage } from './components/ChatMessage.jsx';
 import ChatMessages from './components/ChatMessages.jsx';
 
 import './App.css'
