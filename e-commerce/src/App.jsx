356de083cb33500f870d0240d8c90277051d7cb0
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-06-20 10:32:57 +0700

    feat: implement CheckoutPage component and update routing in App

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="checkout" element={<div>Test checkout Page</div>} />
+      <Route path="checkout" element={<CheckoutPage />} />
     </Routes>
   )
 }
