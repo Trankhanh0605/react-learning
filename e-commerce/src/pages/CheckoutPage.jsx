fd46cf970fe474ea10aac3ffa154acd2e3a85d0e
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-06-20 10:40:13 +0700

    renaming the page by using react title attribute

--- e-commerce/src/pages/CheckoutPage.jsx
+++ e-commerce/src/pages/CheckoutPage.jsx
@@ -1,6 +1,7 @@
 function CheckoutPage() {
   return (
     <>
+    <title>Checkout</title>
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
