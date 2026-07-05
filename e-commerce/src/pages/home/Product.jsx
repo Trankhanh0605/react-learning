730d54340f374c3cc523c9738e78fe1f076d9d6f
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-07-05 14:12:53 +0700

    test clicking add to cart button

--- e-commerce/src/pages/home/Product.jsx
+++ e-commerce/src/pages/home/Product.jsx
@@ -75,6 +75,7 @@ function Product({ product, getCartItems }) {
       </div>
 
       <button className="add-to-cart-button button-primary"
+      data-testid="add-to-cart-button"
         onClick={addToCart}
       >
         Add to Cart
