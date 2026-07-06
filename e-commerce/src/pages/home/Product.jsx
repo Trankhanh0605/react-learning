e339c044de40b36403b4f3695b8792f0aed221c1
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2026-07-06 09:01:21 +0700

    create integration test for homepage

--- e-commerce/src/pages/home/Product.jsx
+++ e-commerce/src/pages/home/Product.jsx
@@ -25,7 +25,8 @@ function Product({ product, getCartItems }) {
   }
   
   return (
-    <div className="product-container">
+    <div className="product-container"
+    data-testid="product-container">
       <div className="product-image-container">
         <img className="product-image"
           data-testid="product-image"
